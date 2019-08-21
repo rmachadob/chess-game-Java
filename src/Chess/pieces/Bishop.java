b437b560e92296a0257f03ee8eef60263aa429f5
Ricardo Barbosa <ricardomachado2112@gmail.com>
2019-08-21 20:46:10 -0300

    Bishop

---
+++ src/Chess/pieces/Bishop.java
@@ -0,0 +1,72 @@
+package Chess.pieces;
+
+import Boardgame.Board;
+import Boardgame.Position;
+import Chess.ChessPiece;
+import Chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);//
+
+	}
+
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);// só pra instanciar com algum valor
+
+		// NW
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);
+		} // esse while vai marcando como true a posição então a peça pode ir. Ele roda
+			// todas as casas VAZIAS
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		} // Esse é pra testar se a peça é inimiga, aí tbm marca a posição como true
+
+		// NE
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// SE
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// SW
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		return mat;
+	}// esse -1 é a linha acima da posição da peça. while: enquanto a posição p
+		// existir e não
+		// tiver uma peça lá, retorna true
+
+}
--- src/Chess/pieces/Pawn.java
+++ src/Chess/pieces/Pawn.java
@@ -13,29 +13,32 @@ public class Pawn extends ChessPiece {
 
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];// matriz auxiliar dentro do método
+
 		Position p = new Position(0, 0);
 
 		if (getColor() == Color.WHITE) {
 			p.setValues(position.getRow() - 1, position.getColumn());
-			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {// se a posição existir e estiver vazia
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() - 2, position.getColumn());
 			Position p2 = new Position(position.getRow() - 1, position.getColumn());
-			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+			// esse if de baixo é pra primeira jpgada quando ele pode andar duas, por isso
+			// testa todas essas condições com ajuda do p2
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
+					&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);// vendo a captura das casas adjacentes
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
-			}			
+			}
 			p.setValues(position.getRow() - 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
-			}	
-			
+			}
+
 			// #specialmove en passant white
 //			if (position.getRow() == 3) {
 //				Position left = new Position(position.getRow(), position.getColumn() - 1);
@@ -47,26 +50,26 @@ public class Pawn extends ChessPiece {
 //					mat[right.getRow() - 1][right.getColumn()] = true;
 //				}
 //			}
-		}
-		else {
+		} else {// else para as pretas(se não é branca é preta) só troca para somar as linhas
 			p.setValues(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 2, position.getColumn());
 			Position p2 = new Position(position.getRow() + 1, position.getColumn());
-			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
+					&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 			p.setValues(position.getRow() + 1, position.getColumn() - 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
-			}			
+			}
 			p.setValues(position.getRow() + 1, position.getColumn() + 1);
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
-			
+
 			// #specialmove en passant black
 //			if (position.getRow() == 4) {
 //				Position left = new Position(position.getRow(), position.getColumn() - 1);
@@ -81,6 +84,7 @@ public class Pawn extends ChessPiece {
 		}
 		return mat;
 	}
+
 	@Override
 	public String toString() {
 		return "P";
