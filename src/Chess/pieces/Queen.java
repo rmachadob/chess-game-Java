9b73cf16f8963922082f26ff1c24c2dae2ec3532
Ricardo Barbosa <ricardomachado2112@gmail.com>
2020-03-23 16:00:40 -0300

    Queen (and Pawn bug fix)

---
+++ src/Chess/pieces/Queen.java
@@ -0,0 +1,114 @@
+package Chess.pieces;
+
+import Boardgame.Board;
+import Boardgame.Position;
+import Chess.ChessPiece;
+import Chess.Color;
+
+public class Queen extends ChessPiece {
+
+	public Queen(Board board, Color color) {
+		super(board, color);//
+
+	}
+
+	@Override
+	public String toString() {
+		return "Q";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);// só pra instanciar com algum valor
+
+		// above - marcar de verdadeiro acima da peça
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+		} // esse while vai marcando como true a posição então a peça pode ir. Ele roda
+			// todas as casas VAZIAS
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		} // Esse é pra testar se a peça é inimiga, aí tbm marca a posição como true
+
+		// left - lógica do de cima mas decrementando as colunas e o que recebe true é a
+		// coluna
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// right - lógica do de cima mas INCREMENTANDO as colunas
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		// below -lógica do de cima só que a linha é +1 ao invés de -1
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// NW
+				p.setValues(position.getRow() - 1, position.getColumn() - 1);
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() - 1, p.getColumn() - 1);
+				} // esse while vai marcando como true a posição então a peça pode ir. Ele roda
+					// todas as casas VAZIAS
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				} // Esse é pra testar se a peça é inimiga, aí tbm marca a posição como true
+
+				// NE
+				p.setValues(position.getRow() - 1, position.getColumn() + 1);
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() - 1, p.getColumn() + 1);
+				}
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				}
+
+				// SE
+				p.setValues(position.getRow() + 1, position.getColumn() + 1);
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() + 1, p.getColumn() + 1);
+				}
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				}
+
+				// SW
+				p.setValues(position.getRow() + 1, position.getColumn() - 1);
+				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+					p.setValues(p.getRow() + 1, p.getColumn() - 1);
+				}
+				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+					mat[p.getRow()][p.getColumn()] = true;
+				}
+
+		return mat;
+	}// esse -1 é a linha acima da posição da peça. while: enquanto a posição p
+		// existir e não
+		// tiver uma peça lá, retorna true
+
+}
